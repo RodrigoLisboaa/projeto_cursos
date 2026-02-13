f6297ff633ae9a8c5d2182eca6174f2c32402c8d
RodrigoLisboaa <rodrigo.s.lisboa7@gmail.com>
2026-02-13 20:30:11 -0300

    feat(logging): adiciona setup de logging

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,9 @@
+from logging_config import setup_logging
 from scripts.load_alunos import main as load_alunos
 from scripts.load_cursos import main as load_cursos
 
 def main() -> None:
+    setup_logging()
     load_alunos()
     load_cursos()
 
