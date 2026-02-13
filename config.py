a9d352648b22826069c8bb6279402f755638ecb4
RodrigoLisboaa <rodrigo.s.lisboa7@gmail.com>
2026-02-13 16:17:37 -0300

    chore(config): adiciona config central e .env.example

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CSV_DIR = os.getenv("CSV_DIR", "data")
+
+PGHOST = os.getenv("PGHOST", "localhost")
+PGPORT = int(os.getenv("PGPORT", 5432))
+PGDATABASE = os.getenv("PGDATABASE", "mentoria_dev")
+PGUSER = os.getenv("PGUSER", "postgres")
+PGPASSWORD = os.getenv("PGPASSWORD", "")
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    f"postgresql://{PGUSER}:{PGPASSWORD}@{PGHOST}:{PGPORT}/{PGDATABASE}",
+)
